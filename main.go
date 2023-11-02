498ec0b9c4089bfad3fc51c55ea830a58cfa1b01
CosmicBagel <>
2023-11-02 15:46:03 -0600

    inital commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	println("hello world")
+
+	/*
+		- http serve
+		- hello world html?
+			- serve page html? (templates?)
+			- serve static js/css/html?
+		- routes for get and post
+		- send post to db
+		- get from db
+	*/
+
+}
