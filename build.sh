e2cb931eca05df2670c1742036a450ef7dcb13c0
CosmicBagel <>
2023-11-23 17:18:33 -0700

    Building staticly linked binaries Need to be able to deploy binary to minmal containers missing dependencies

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 SCRIPT_PATH=$(dirname $(realpath $0))
 cd $SCRIPT_PATH/api/pkg
-go build -o ../container_files/chronoBro
+# note: doing a fully static build so exe can be deployed on minimal images, or 
+# images not using clib
+CGO_ENABLED=0
+go build -o ../container_files/chronoBro -ldflags "-linkmode 'external' -extldflags '-static'"
