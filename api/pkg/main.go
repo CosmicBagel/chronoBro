7a75fd50b1b73ec854e7993107a5f5bce0eb1740
CosmicBagel <>
2023-11-23 17:19:12 -0700

    Minimal hello world http example

--- api/pkg/main.go
+++ api/pkg/main.go
@@ -1,8 +1,15 @@
 package main
 
+import (
+	"net/http"
+	"time"
+)
+
 func main() {
 
-    println("hello world")
+	println("hello world")
+	println("lookie")
+
 	/*
 		- http serve
 		- hello world html?
@@ -13,4 +20,22 @@ func main() {
 		- get from db
 	*/
 
+	s := http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 90 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      http.HandlerFunc(hello),
+	}
+
+	err := s.ListenAndServe()
+	if err != nil {
+		if err != http.ErrServerClosed {
+			panic(err)
+		}
+	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("fsdafsadfasfd\n"))
 }
