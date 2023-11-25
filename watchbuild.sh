a65e3336c2ddf8d9ac87143550e9a51cc356e3e2
CosmicBagel <>
2023-11-25 13:34:28 -0700

    Now invoking dev-tooling container to build

--- watchbuild.sh
+++ watchbuild.sh
@@ -7,6 +7,6 @@ cd $SCRIPT_PATH/api/pkg
 inotifywait -m . -e modify -e move -e create -e delete |
     while read -r directory action file; do
         if [[ $file =~ .*go$ ]]; then
-            ../../build.sh
+            docker exec -it chronobro-dev-tooling-1 ash /build.sh
         fi
     done
