9f2b11314514fed23bf7224a8578712a145e933a
CosmicBagel <>
2023-11-29 15:13:18 -0700

    Added 404 routing as well as changed around route building

--- api/pkg/main.go
+++ api/pkg/main.go
@@ -3,33 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 )
+
+// post (create), get (read), put (update), delete
+// https://chrono-api.cosmicbagel.com/v1/events/{id}
+// domain/service/objectReceivingAction/action/version
+
+// /events/{id} [get, put, delete]
+// /events [post]
+
+// db events table
+// id (int64), start (datetime), end (datetime), title (varchar), description (varchar)
 
 func main() {
 
 	println("hello world")
 	println("lookie")
 
-	/*
-		- http serve
-		- hello world html?
-			- serve page html? (templates?)
-			- serve static js/css/html?
-		- routes for get and post
-		- send post to db
-		- get from db
-	*/
-
-	// mux := http.NewServeMux()
-	// mux.Handle("", )
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/events/", eventsPathHandler)
+	mux.HandleFunc("/", rootPathHandler)
 
 	s := http.Server{
 		Addr:         ":8080",
 		ReadTimeout:  30 * time.Second,
 		WriteTimeout: 90 * time.Second,
 		IdleTimeout:  120 * time.Second,
-		Handler:      http.HandlerFunc(hello),
+		Handler:      mux,
 	}
 
 	err := s.ListenAndServe()
@@ -40,10 +43,45 @@ func main() {
 	}
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-    _, err := fmt.Fprintf(w, "hello world %s\n", r.Method)
-    if err != nil {
-        panic(err)
-    }
+func eventsPathHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/v1/events/")
+
+	/*//
+	now at the point where I need to check both http verb
+	and the path, and optional subpaths
+	nested resources (eg author/19/posts) also need to be
+	considered as potential optional subpaths
+	use http.StripPrefix handler wrapper maybe
+
+	//*/
+
+	if len(idStr) == 0 {
+		
+	}
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		w.WriteHeader(400)
+		fmt.Fprintf(w, "400 bad request")
+		return
+	}
+
+	_, err = fmt.Fprintf(w, "events %d\n", id)
+	if err != nil {
+		panic(err)
+	}
+
 }
-// domain/service/objectReceivingAction/action/version
+
+func rootPathHandler(w http.ResponseWriter, r *http.Request) {
+	// matches everything that doesn't match a handled path
+	// so if path is not literally root "/", then it is 404
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	_, err := fmt.Fprintf(w, "hello world %s\n", r.Method)
+	if err != nil {
+		panic(err)
+	}
+}
